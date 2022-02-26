8b7bde55f65256cc8420beeafab5e96e022f3a1f
sun00120 <sun00120@algonquinlive.com>
2022-02-26 06:37:32 -0500

    update Course model and Student model

--- models/Course.js
+++ models/Course.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-  code: String,
-  title: String,
-  description: String,
-  url: String,
+  code: { type: String, required: true, maxlength: 16 },
+  title: { type: String, required: true, maxlength: 255 },
+  description: { type: String, required: false, maxlength: 2048 },
+  url: { type: String, required: false, maxlength: 512 },
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
 });
 
 const Model = mongoose.model("Course", schema);
--- models/Student.js
+++ models/Student.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  nickName: String,
-  email: String,
+  firstName: { type: String, required: true, maxlength: 64 },
+  lastName: { type: String, required: true, maxlength: 64 },
+  nickName: { type: String, required: false, maxlength: 64 },
+  email: { type: String, required: true, maxlength: 512 },
 });
 
 const Model = mongoose.model("Student", schema);
