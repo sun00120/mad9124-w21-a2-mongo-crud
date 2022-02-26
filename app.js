aeb368fba55341dbb803fc618aa2536524bde74a
sun00120 <sun00120@algonquinlive.com>
2022-02-26 04:15:27 -0500

    Initialize project folder, add Mongoose db connection to Express app

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// Don't forget to use NPM to install Express and Mongoose.
+"use strict";
+
+// load dependencies
+const express = require("express");
+const studentRouter = require("./routes/student.js");
+const courseRouter = require("./routes/course.js");
+
+// create the express app
+const app = express();
+
+// configure express middleware
+app.use(express.json());
+app.use("/api/students", studentRouter);
+app.use("/api/courses", courseRouter);
+
+const port = process.env.port || 3030;
+app.listen(port, () => console.log(`Server listening on port ${port} ...`));
